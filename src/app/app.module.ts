2a0c0764c50b1e5e85036e40bf4263e1036f2f2e
Lucas Guimarães <lukahbg@gmail.com>
2022-09-12 23:59:19 -0300

    [feat]: added mat-grid component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatTableModule} from '@angular/material/table';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { FormsModule } from '@angular/forms';
 import { DisabledInput } from './components/disabled-input/disabled-input.component';
 import { InputComponent } from './components/input/input.component';
-import { ConverterApiService } from './services/converter-api.service'; 
+import { ConverterApiService } from './services/converter-api.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DisabledInput,
-    InputComponent
-  ],
+  declarations: [AppComponent, DisabledInput, InputComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -32,10 +29,11 @@ import { HttpClientModule } from '@angular/common/http';
     MatTableModule,
     MatInputModule,
     MatIconModule,
+    MatGridListModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [ConverterApiService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
